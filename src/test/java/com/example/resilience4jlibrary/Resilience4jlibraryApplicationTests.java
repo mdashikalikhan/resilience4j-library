96d05d6d23ae8516dce49c0b993b475a8b8d4d23
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2025-01-14 00:08:44 +0600

    Initialize

---
+++ src/test/java/com/example/resilience4jlibrary/Resilience4jlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resilience4jlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
